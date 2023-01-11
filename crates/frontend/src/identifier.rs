1ea0e4f032881d4bdbfeb55b20596ab9f20e647e
Tristan Fox <tristanfox009@outlook.com>
2023-01-11 14:31:18 +1100

    feat: allow underscores in identifiers

--- crates/frontend/src/identifier.rs
+++ crates/frontend/src/identifier.rs
@@ -1,4 +1,4 @@
-use nom::character::complete::alpha1;
+use nom::bytes::complete::take_while1;
 
 use crate::Parse;
 
@@ -7,7 +7,7 @@ pub struct Identifier(pub String);
 
 impl Parse for Identifier {
     fn parse(input: &str) -> nom::IResult<&str, Self> {
-        let (input, id) = alpha1(input)?;
+        let (input, id) = take_while1(|x: char| x.is_alphabetic() || x == '_')(input)?;
         Ok((input, Self(id.to_string())))
     }
 }
